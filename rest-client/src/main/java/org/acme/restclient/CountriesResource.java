2333c5e26bfe9d4691d083cb1209cd6b5e2bb428
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2019-03-07 18:03:25 +0100

    rest client using builder

--- rest-client/src/main/java/org/acme/restclient/CountriesResource.java
+++ rest-client/src/main/java/org/acme/restclient/CountriesResource.java
@@ -1,27 +1,25 @@
 package org.acme.restclient;
 
-import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
 
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.net.URI;
 import java.util.Set;
 
 @Path("/country")
 public class CountriesResource {
 
-    @Inject
-    @RestClient
-    CountriesService countriesService;
-
-
     @GET
     @Path("/name/{name}")
     @Produces(MediaType.APPLICATION_JSON)
     public Set<Country> name(@PathParam("name") String name) {
-        return countriesService.getByName(name);
+        return RestClientBuilder.newBuilder()
+                .baseUri(URI.create("https://restcountries.eu/rest"))
+                .build(CountriesService.class)
+                .getByName(name);
     }
 }
--- rest-client/src/main/java/org/acme/restclient/CountriesService.java
+++ rest-client/src/main/java/org/acme/restclient/CountriesService.java
@@ -1,7 +1,5 @@
 package org.acme.restclient;
 
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -9,7 +7,6 @@ import javax.ws.rs.Produces;
 import java.util.Set;
 
 @Path("/v2")
-@RegisterRestClient
 public interface CountriesService {
 
     @GET
